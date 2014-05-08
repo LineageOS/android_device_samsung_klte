41294be57210904c9cc27a6f4d8f4ee1dd162e9c
Tom Marshall <tdm@cyngn.com>
2014-05-08 14:10:42 -0700

    Fix recovery key file for use with latest CWM
    Change-Id: Ibbbf3c953af547c1bdcb177c16f9650e127356f6

--- recovery/recovery_keys.c
+++ recovery/recovery_keys.c
@@ -5,18 +5,6 @@
 #include "extendedcommands.h"
 
 
-int device_toggle_display(volatile char* key_pressed, int key_code) {
-    int alt = key_pressed[KEY_LEFTALT] || key_pressed[KEY_RIGHTALT];
-    if (alt && key_code == KEY_L)
-        return 1;
-    // allow toggling of the display if the correct key is pressed, and the display toggle is allowed or the display is currently off
-    if (ui_get_showing_back_button()) {
-        return 0;
-        //return get_allow_toggle_display() && (key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_END);
-    }
-    return get_allow_toggle_display() && (key_code == KEY_HOMEPAGE || key_code == KEY_MENU || key_code == KEY_POWER || key_code == KEY_END);
-}
-
 int device_handle_key(int key_code, int visible) {
     if (visible) {
         switch (key_code) {
@@ -31,25 +19,14 @@ int device_handle_key(int key_code, int visible) {
             case KEY_VOLUMEUP:
                 return HIGHLIGHT_UP;
             case KEY_HOMEPAGE:
-                if (ui_get_showing_back_button()) {
-                    return SELECT_ITEM;
-                }
-                if (!get_allow_toggle_display())
-                    return GO_BACK;
-                break;
             case KEY_POWER:
-                if (ui_get_showing_back_button()) {
-                    return SELECT_ITEM;
-                }
-                if (!get_allow_toggle_display())
-                    return GO_BACK;
-                break;
             case KEY_LEFTBRACE:
             case KEY_ENTER:
             case BTN_MOUSE:
             case KEY_CAMERA:
             case KEY_F21:
-            case KEY_SEND:            
+            case KEY_SEND:
+                return SELECT_ITEM;
             case KEY_END:
             case KEY_BACKSPACE:
             case KEY_SEARCH:
