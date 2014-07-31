65493d2bf7f0d6fc2c657cc21fbde54253e7de4b
Ethan Chen <intervigil@gmail.com>
2014-07-31 16:40:13 -0700

    klte: Use prebuilt GPS libs

--- device.mk
+++ device.mk
@@ -41,12 +41,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # GPS
-PRODUCT_PACKAGES += \
-    gps.msm8974
-
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/etc/gps.conf:/system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/etc/sap.conf:/system/etc/sap.conf
+    $(LOCAL_PATH)/configs/flp.conf:/system/etc/flp.conf \
+    $(LOCAL_PATH)/configs/gps.conf:/system/etc/gps.conf \
+    $(LOCAL_PATH)/configs/sap.conf:/system/etc/sap.conf
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
