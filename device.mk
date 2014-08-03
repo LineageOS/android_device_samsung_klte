37d5acff7807544e4520d374362130c3b99219a8
Ethan Chen <intervigil@gmail.com>
2014-08-03 15:10:21 -0700

    klte: Build GPS HAL

--- device.mk
+++ device.mk
@@ -53,6 +53,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # GPS
+PRODUCT_PACKAGES += \
+    gps.msm8974
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/flp.conf:/system/etc/flp.conf \
     $(LOCAL_PATH)/configs/gps.conf:/system/etc/gps.conf \
