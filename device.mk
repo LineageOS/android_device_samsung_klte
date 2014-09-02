4f63ec20bbdb3f522d274031f4eb572bff715a59
Matt Filetto <matt.filetto@gmail.com>
2014-09-02 06:20:25 -0700

    klte: add libnfc-brcm.conf
    * We are using the prebuilt NFC HAL now and it requires this to function
    
    * Samsung package version: G900FXXU1ANG2_G900FOXA1ANG3_BTU
    
    Change-Id: I09fb779c22eef56e24d6e0cf77dce4407c73d720

--- device.mk
+++ device.mk
@@ -24,6 +24,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # NFC
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
     $(LOCAL_PATH)/configs/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
     $(LOCAL_PATH)/configs/nfcee_access.xml:system/etc/nfcee_access.xml
 
