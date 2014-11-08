adf94436247bcc325dc14a24b49391e5ec564f4d
jumoog <kilianvonpflugk@gmail.com>
2014-11-08 16:48:20 +0100

    init add klteduosxx (G900FD Dualsim)
    Change-Id: If40f44edd3bc3fc68e5d9761161b612876eb1d01

--- init/init_klte.c
+++ init/init_klte.c
@@ -82,6 +82,13 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.product.model", "SM-G900A");
         property_set("ro.product.device", "klteatt");
         gsm_properties();
+    } else if (strstr(bootloader, "G900FD")) {
+        /* klteduosxx */
+        property_set("ro.build.fingerprint", "samsung/klteduosxx/klte:4.4.2/KOT49H/G900FDXXU1ANJ1:user/release-keys");
+        property_set("ro.build.description", "klteduosxx-user 4.4.2 KOT49H G900FDXXU1ANJ1 release-keys");
+        property_set("ro.product.model", "SM-G900FD");
+        property_set("ro.product.device", "klteduosxx");
+        gsm_properties();
     } else {
         /* kltexx */
         property_set("ro.build.fingerprint", "samsung/kltexx/klte:4.4.2/KOT49H/G900FXXU1ANG2:user/release-keys");
