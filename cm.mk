273dc4005d9f9a5097710680849cf674b868c31d
Ethan Chen <intervigil@gmail.com>
2014-11-23 03:04:15 -0800

    klte: Re-order product inheritance
    Change-Id: I70a55ed27fd3eebdce864a8d2cdada6c24eb9c5b

--- cm.mk
+++ cm.mk
@@ -1,10 +1,8 @@
-$(call inherit-product, device/samsung/klte/full_klte.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, device/samsung/klte/full_klte.mk)
 
 PRODUCT_DEVICE := klte
 PRODUCT_NAME := cm_klte
