3588b5d8d6abf47ef6468fca4dd593e3a54eec46
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2016-08-27 11:03:32 +0200

    klte: Use generic extract scripts
    Change-Id: Iea341e5480b1e2d72b673ea75067621acd42cb58

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,7 +1,24 @@
-#!/bin/sh
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 set -e
 
-export VENDOR=samsung
 export DEVICE=klte
-./../../$VENDOR/klte-common/setup-makefiles.sh $@
+export DEVICE_COMMON=klte-common
+export VENDOR=samsung
+
+./../$DEVICE_COMMON/setup-makefiles.sh $@
