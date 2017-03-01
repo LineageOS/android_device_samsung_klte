f3be99c2362a2fbb0c220a77ae7235bdb678d22f
Kevin F. Haggerty <haggertk@lineageos.org>
2017-02-28 17:21:35 -0700

    klte: Use klte-common releasetools extension
    Change-Id: I9cd6175c58b1a3a012f94b8847426cb93c885936

--- device.mk
+++ device.mk
@@ -36,5 +36,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
     $(LOCAL_PATH)/configs/nfcee_access.xml:system/etc/nfcee_access.xml
 
+# Variant blobs script
+PRODUCT_COPY_FILES += \
+    device/samsung/klte-common/releasetools/variant_blobs.sh:install/bin/variant_blobs.sh
+
 # common klte
 $(call inherit-product, device/samsung/klte-common/klte.mk)
