fd4a9fd44e8f464a6595e416637bf9c1e7c189f5
Luca Stefani <luca020400@lineageos.org>
2017-08-18 11:39:28 +0200

    klte: init: Update for O
    Change-Id: I60475e3dbf68329a46c421e8c8465a4ea2e28203

--- init/init_klte.cpp
+++ init/init_klte.cpp
@@ -31,12 +31,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "vendor_init.h"
+#include <android-base/properties.h>
+
 #include "property_service.h"
+#include "vendor_init.h"
 #include "log.h"
-#include "util.h"
 
 #include "init_msm8974.h"
+
+using android::base::GetProperty;
 
 void cdma_properties(char const *operator_alpha,
         char const *operator_numeric,
@@ -61,11 +64,11 @@ void gsm_properties()
 
 void init_target_properties()
 {
-    std::string platform = property_get("ro.board.platform");
+    std::string platform = GetProperty("ro.board.platform", "");
     if (platform != ANDROID_TARGET)
         return;
 
-    std::string bootloader = property_get("ro.bootloader");
+    std::string bootloader = GetProperty("ro.bootloader", "");
 
     if (bootloader.find("G900AZ") == 0) {
         /* klteaio - Cricket MVNO */
@@ -136,6 +139,7 @@ void init_target_properties()
         gsm_properties();
     }
 
-    std::string device = property_get("ro.product.device");
-    INFO("Found bootloader id %s setting build properties for %s device\n", bootloader.c_str(), device.c_str());
+    std::string device = GetProperty("ro.product.device", "");
+    LOG(INFO) << "Found bootloader id " << bootloader <<  " setting build properties for "
+	    << device <<  " device" << std::endl;
 }
