e3609d5483f71d297d0c000252528e5b5b1576de
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2017-10-06 19:00:42 +0300

    [DNM] klte: O bringup
    Change-Id: I2fcd62b1dd6cbe292d89cf97085c9549bf69ba14

--- lineage.mk
+++ lineage.mk
@@ -1,6 +1,8 @@
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/klte/full_klte.mk)
+
+PRODUCT_SKIP_FINGERPRINT_FROM_FILE := true
 
 PRODUCT_DEVICE := klte
 PRODUCT_NAME := lineage_klte
