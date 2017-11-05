87058b088c17ecdf2fc223e0250850f0064e3c18
Kevin F. Haggerty <haggertk@lineageos.org>
2017-11-05 13:28:59 -0700

    klte: Move NFC configs to vendor/
    Change-Id: If7d3e63ec05db76456d34d245fd938b071f78bb6

--- device.mk
+++ device.mk
@@ -32,9 +32,9 @@ PRODUCT_PACKAGES += \
     nfc_nci.msm8974
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-    $(LOCAL_PATH)/configs/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
-    $(LOCAL_PATH)/configs/nfcee_access.xml:system/etc/nfcee_access.xml
+    $(LOCAL_PATH)/configs/libnfc-brcm.conf:system/vendor/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/configs/libnfc-nxp.conf:system/vendor/etc/libnfc-nxp.conf \
+    $(LOCAL_PATH)/configs/nfcee_access.xml:system/vendor/etc/nfcee_access.xml
 
 # common klte
 $(call inherit-product, device/samsung/klte-common/klte.mk)
