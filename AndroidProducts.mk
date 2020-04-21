5cfe7a2088e272f7442a1b4a60887bc713210bd8
Can KIZILYAR <50930823+cnkzlyr@users.noreply.github.com>
2020-04-21 20:16:46 +0300

    add many custom rom's support

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,3 +15,81 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_klte.mk
+    $(LOCAL_DIR)/aicp_klte.mk \
+    $(LOCAL_DIR)/aim_klte.mk \
+    $(LOCAL_DIR)/aokp_klte.mk \
+    $(LOCAL_DIR)/aosip_klte.mk \
+    $(LOCAL_DIR)/aosp_klte.mk \
+    $(LOCAL_DIR)/bootleg_klte.mk \
+    $(LOCAL_DIR)/derp_klte.mk \
+    $(LOCAL_DIR)/du_klte.mk \
+    $(LOCAL_DIR)/havoc_klte.mk \
+    $(LOCAL_DIR)/lineage_klte.mk \
+    $(LOCAL_DIR)/rr_klte.mk \
+    $(LOCAL_DIR)/superior_klte.mk \
+    $(LOCAL_DIR)/xtended_klte.mk
+
+COMMON_LUNCH_CHOICES := \
+    aicp_klte-user \
+    aicp_klte-userdebug \
+    aicp_klte-eng
+
+COMMON_LUNCH_CHOICES := \
+    aim_klte-user \
+    aim_klte-userdebug \
+    aim_klte-eng
+
+COMMON_LUNCH_CHOICES := \
+    aokp_klte-user \
+    aokp_klte-userdebug \
+    aokp_klte-eng
+
+COMMON_LUNCH_CHOICES := \
+    aosip_klte-user \
+    aosip_klte-userdebug \
+    aosip_klte-eng
+
+COMMON_LUNCH_CHOICES := \
+    aosp_klte-user \
+    aosp_klte-userdebug \
+    aosp_klte-eng
+
+COMMON_LUNCH_CHOICES := \
+    bootleg_klte-user \
+    bootleg_klte-userdebug \
+    bootleg_klte-eng
+
+COMMON_LUNCH_CHOICES := \
+    derp_klte-user \
+    derp_klte-userdebug \
+    derp_klte-eng
+
+COMMON_LUNCH_CHOICES := \
+    du_klte-user \
+    du_klte-userdebug \
+    du_klte-eng
+
+COMMON_LUNCH_CHOICES := \
+    havoc_klte-user \
+    havoc_klte-userdebug \
+    havoc_klte-eng
+
+COMMON_LUNCH_CHOICES := \
+    lineage_klte-user \
+    lineage_klte-userdebug \
+    lineage_klte-eng
+
+COMMON_LUNCH_CHOICES := \
+    rr_klte-user \
+    rr_klte-userdebug \
+    rr_klte-eng
+
+COMMON_LUNCH_CHOICES := \
+    superior_klte-user \
+    superior_klte-userdebug \
+    superior_klte-eng
+
+COMMON_LUNCH_CHOICES := \
+    xtended_klte-user \
+    xtended_klte-userdebug \
+    xtended_klte-eng
